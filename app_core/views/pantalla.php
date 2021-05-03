95b97fdbb01d0b30d660ab9a437d812e31901abd
KevinAH <colo7112012@gmail.com>
2021-05-03 11:23:20 -0600

    add view pantalla

---
+++ app_core/views/pantalla.php
@@ -0,0 +1,44 @@
+<div id='page'>
+		<header>
+		</header>
+
+		<section id='main'>
+			<section id='device'>
+				<div id='screen'>
+					
+					<div id='main_screen'>
+					<button id="btn_buscar" width="20" height="15" class="icon"><i class="fa fa-cog"></i></button>
+						<div id='topbar'><span id='lbl_time_top'>HOLAA</span></div>
+						<div id='panel'>
+							<div id='panel_icons'>
+
+								<div id='pixel_art_app' class='icons' >
+									<div class='label_icon'>PixelArt</div>
+								</div>
+
+								<div id='bd_productos_app' class='icons' >
+									<div class='label_icon'>Productos</div>
+								</div>
+
+								<div id='twitter_app' class='icons' >
+									<div class='label_icon'>Twitter</div>
+								</div>
+								<div id='0' class='iconsFondo'></div>
+								<div id='1' class='iconsFondo'></div>
+								<div id='2' class='iconsFondo'></div>
+								<div id='3' class='iconsFondo'></div>
+								<div id='4' class='iconsFondo'></div>
+								<div id='5' class='iconsFondo'></div>
+							</div>
+						</div>
+						<div id='main_app'><object id="window_app" data="" width="100%" height="100%"></object></div>
+						<div id='main_menu'></div>
+					</div>
+				</div>
+				
+			</section>
+		</section>
+
+		<footer>
+		</footer>
+	</div>
