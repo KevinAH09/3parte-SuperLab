37519a6ddd222c31585ae0283f86a0a9792769cf
Bosco <bosco11jico100@gmail.com>
2021-05-05 22:29:30 -0600

    g
    g

--- app_design/js/funcionesProducto.js
+++ app_design/js/funcionesProducto.js
@@ -1,12 +1,6 @@
 
+window.addEventListener('load', MyChart, false);
 
-window.onload = function () {
-  MyChart();
-}
-
-function hola() {
-  console.log("gh");
-}
 
 function cargarProductos(dato) {
 
@@ -114,7 +108,7 @@ function cargarDatosEnFormulario(codigo) {
       document.getElementById('txt_cant').value = myData.cant;
       document.getElementById('txt_venc').value = myData.venc;
       document.getElementById('txt_prov').value = myData.prov;
-      console.log(myData);
+
 
     })
     .catch(function (error) { //En caso de carga fallida del recurso
@@ -154,21 +148,128 @@ function MyChart() {
       myData = JSON.parse(aux.textContent);
       var nombre = [];
       var precio = [];
-      console.log(myData);
+      var cantidad = [];
+      var proveedor = [];
       for (var i = 0; i < myData.length; i++) {
         nombre.push(myData[i].nom);
         precio.push(myData[i].prec);
-
+        cantidad.push(myData[i].cant);
+        proveedor.push(myData[i].prov);
       }
-      
+
+      var ProvCant = [];
+      var ProvP = [];
+      var dato1;
+      var dato2 = 0;
+      var aux = proveedor.length;
+      for (var k = 0; k <= proveedor.length; k++) {
+        dato2 = 0;
+        dato1 = [];
+        dato1 = proveedor.shift();
+        for (var j = 0; j < proveedor.length; j++) {
+
+          if (dato1 == proveedor[j]) {
+            proveedor.splice(j, 1);
+            j = j - 1;
+            dato2++;
+          }
+        }
+        k = 0;
+        dato2++;
+        ProvCant.push(dato2);
+        ProvP.push(dato1);
+      }
+      var procentajes = [];
+      for (var h = 0; h <= ProvCant.length; h++) {
+        procentajes.push((ProvCant.shift() * 100) / aux);
+        h = 0;
+      }
       var ctx = document.getElementById('myChart');
-      var myChart = new Chart(ctx, {
+      var ctxLineal = document.getElementById('myChartLineal');
+      var ctxCircular = document.getElementById('myChartCircular');
+
+      new Chart(ctxCircular, {
+        type: 'pie',
+        data: {
+          labels: ProvP,
+          datasets: [{
+
+            data: procentajes,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+            ],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }]
+        },
+        options: {
+          plugins: {
+            datalabels:
+            {
+              color:'#fff'
+            },
+            title: {
+              display: true,
+              text: 'Gráfico #3, Todos los proveedores y el porcentaje de los productos que se aportan al inventario',
+            }
+          }
+        }
+        // formatter: (procentajes) => {
+        //   console.log(procentajes);
+        //   return procentajes.value + '%';
+        // }
+      });
+
+      new Chart(ctxLineal, {
+        type: 'line',
+        data: {
+          labels: nombre,
+          datasets: [{
+            label: 'Gráfico #1, Todos los productos con sus cantidades',
+            data: cantidad,
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+          }]
+        },
+        options: {
+          plugins: {
+            legend: false,
+            title: {
+              display: true,
+              text: 'Gráfico #1, Todos los productos con sus cantidades',
+            }
+          },
+          scales: {
+            y: {
+              display: true,
+              title: {
+                display: true,
+                text: 'Cantidades',
+                color: '#911',
+                font: {
+                  family: 'Times',
+                  size: 20,
+                  style: 'normal',
+                  lineHeight: 1.2
+                }
+              },
+              beginAtZero: true
+            }
+          }
+        },
+      });
+
+      new Chart(ctx, {
         type: 'bar',
         data: {
 
           labels: nombre,
           datasets: [{
-            label: '# of Votes',
             data: precio,
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
@@ -190,8 +291,28 @@ function MyChart() {
           }]
         },
         options: {
+          legend: false,
+          plugins: {
+            legend: false,
+            title: {
+              display: true,
+              text: 'Gráfico #2, Todos los productos con sus precios',
+            }
+          },
           scales: {
             y: {
+              display: true,
+              title: {
+                display: true,
+                text: 'Precios',
+                color: '#911',
+                font: {
+                  family: 'Times',
+                  size: 20,
+                  style: 'normal',
+                  lineHeight: 1.2
+                }
+              },
               beginAtZero: true
             }
           }
